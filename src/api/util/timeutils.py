af57260b8d0d0983c4fa3664094090f43f1be6e9
dmitrii <sokolovdp@gmail.com>
2018-10-16 18:51:03 +0300

    initial commit with python 3.6 style corrections

--- src/api/util/timeutils.py
+++ src/api/util/timeutils.py
@@ -1,4 +1,6 @@
 import datetime
+
+
 # Original fix for Py2.6: https://github.com/mozilla/mozdownload/issues/73
 def total_seconds(td):
     # Keep backward compatibility with Python 2.6 which doesn't have
@@ -6,12 +8,13 @@ def total_seconds(td):
     if hasattr(td, 'total_seconds'):
         return td.total_seconds()
     else:
-        return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+        return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10 ** 6) / 10 ** 6
+
 
 def convert_to_epoch(timestamp):
-        if (type(timestamp) is datetime.date):
-            timestamp = datetime.datetime.fromordinal(timestamp.toordinal())
-        timestamp = timestamp.replace(tzinfo=None)
-	diff = (timestamp - datetime.datetime(1970, 1, 1))
-	seconds = int(total_seconds(diff))
-	return seconds
+    if (type(timestamp) is datetime.date):
+        timestamp = datetime.datetime.fromordinal(timestamp.toordinal())
+    timestamp = timestamp.replace(tzinfo=None)
+    diff = (timestamp - datetime.datetime(1970, 1, 1))
+    seconds = int(total_seconds(diff))
+    return seconds
